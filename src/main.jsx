26c94b6fbd6b7d2dd2b4ee5df4b0e85de853d499
Jakub Anczykowski <jakubanczykowski@MacBook-Pro-Jakub.local>
2025-04-03 09:50:38 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GPSScrollAnimation from './GPSScrollAnimation'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GPSScrollAnimation />
+  </React.StrictMode>
+)
